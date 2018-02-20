d17a4def6824c946099b54af646cd0bd774800be
Nicola Bortolan <nicolabortolan@gmail.com>
2018-02-20 20:14:55 +0100

    Finish graphic

--- app/src/main/java/com/therealnic/tris/MainActivity.java
+++ app/src/main/java/com/therealnic/tris/MainActivity.java
@@ -2,12 +2,35 @@ package com.therealnic.tris;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
